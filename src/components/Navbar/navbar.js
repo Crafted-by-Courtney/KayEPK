7eddf9765b4267033fd0d221663d96b010e01080
Courtney Cook <courtneycook82@gmail.com>
2024-05-14 06:34:11 -0400

    mostly css changes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,31 +1,119 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./navbar.css";
-import kaylogo from "../images/kaylogo.png";
-import { Link } from 'react-scroll';
-import menu from "../images/hamburgerMenu.png"
+import kaylogo2 from "../images/kaylogo2.png";
+import { Link } from "react-scroll";
+import menu from "../images/hamburgerMenu.png";
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
-    <nav className='navbar'>
-        <img src={kaylogo} alt='Logo' className='logo'/>
-        <div className='desktopMenu'>
-            <Link activeClass='active' to='intro'spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
-            <Link activeClass='active' to='skills'spy={true} smooth={true} offset={-50} duration={500}className='desktopMenuListItem'>About</Link>
-            <Link activeClass='active' to='works'spy={true} smooth={true} offset={-50} duration={500}className='desktopMenuListItem'>Music</Link>
-        </div>
-        <button type="button" className='desktopMenuBtn' onClick= {() => {document.getElementById('contact').scrollIntoView({behavior: 'smooth'});}}>Let's Connect
-        </button>
+    <nav className="navbar">
+      <img src={kaylogo2} alt="Logo" className="logo" />
+      <div className="desktopMenu">
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Music
+        </Link>
+      </div>
+      <button
+        type="button"
+        className="desktopMenuBtn"
+        onClick={() => {
+          document
+            .getElementById("contact")
+            .scrollIntoView({ behavior: "smooth" });
+        }}
+      >
+        Let's Connect
+      </button>
 
-        <img src={menu} alt='logo' className='mobMenu' onClick={()=>setShowMenu(!showMenu)}/>
-        <div className='navMenu' style={{display: showMenu? 'flex': 'none'}}>
-            <Link activeClass='active' to='intro'spy={true} smooth={true} offset={-50} duration={500} className='listItem' onClick={()=>setShowMenu(false)}>Home</Link>
-            <Link activeClass='active' to='skills'spy={true} smooth={true} offset={-50} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>About</Link>
-            <Link activeClass='active' to='works'spy={true} smooth={true} offset={-50} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Music</Link>
-            <Link activeClass='active' to='contact'spy={true} smooth={true} offset={-50} duration={500}className='listItem' onClick={()=>setShowMenu(false)}>Contact</Link>
-        </div>
+      <img
+        src={menu}
+        alt="logo"
+        className="mobMenu"
+        onClick={() => setShowMenu(!showMenu)}
+      />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Music
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          className="listItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,28 +1,51 @@
-import React from 'react';
+import React from "react";
 import "./works.css";
-import goodfriends from '../images/goodfriends.jpg';
-import grave from '../images/grave.jpg';
-import aswelay from '../images/aswelay.jpg';
-import oneheart from '../images/oneheart.jpg';
-import goaldigger from '../images/goaldigger.jpg';
-import threeshots from '../images/threeshots.jpg';
+import goodfriends from "../images/goodfriends.jpg";
+import grave from "../images/grave.jpg";
+import aswelay from "../images/aswelay.jpg";
+import oneheart from "../images/oneheart.jpg";
+import goaldigger from "../images/goaldigger.jpg";
+import threeshots from "../images/threeshots.jpg";
 
 const Works = () => {
   return (
-    <section id = 'works'>
-        <h2 className='worksTitle'>My Music</h2>
-        <span className='workDesc'>Welcome to the sonic realm of <span className='introName'><span>Kiki</span></span>, where music transcends boundaries and emotions find their melody. — a diverse collection of soulful melodies, powerful lyrics, and innovative compositions that paint a vibrant picture of artistic expression. Explore the melodies that define moments and narratives that resonate, as Kiki invites you on a captivating auditory experience through this portfolio.</span>
-        <div className='worksImgs'>
-            <a href='https://music.apple.com/us/album/good-friends-single/1552929337'><img src={goodfriends} alt='' className='worksImg' /></a>
-            <a href='https://music.apple.com/us/album/grave-single/1509413037'><img src={grave} alt='' className='worksImg' /></a>
-            <a href='https://open.spotify.com/album/4x2FrVcLXptxSMb8tEtZMv?si=jamktjeCQP2O56UPgxrQ3A'><img src={aswelay} alt='' className='worksImg' /></a>
-            <a href='https://music.apple.com/us/album/one-heart/1671896308'><img src={oneheart} alt='' className='worksImg' /></a>
-            <a href='https://open.spotify.com/album/1ypCJdz6cOEtXuMnCATIgH?si=kGWec0RNR1us3yxah5Gvpg'><img src={goaldigger} alt='' className='worksImg' /></a>
-            <a href='https://open.spotify.com/album/19UQkovdPfdIpSE5XyAHg0?si=idE-DV8cQrWeREWii1XWBA'><img src={threeshots} alt='' className='worksImg' /></a>
-        </div>
-        <button className='worksBtn'>See More</button>
+    <section id="works">
+      <h2 className="worksTitle">My Music</h2>
+      <span className="worksDesc">
+        Welcome to the sonic realm of{" "}
+        <span className="introName">
+          <span>Kiki</span>
+        </span>
+        , where music transcends boundaries and emotions find their melody. — a
+        diverse collection of soulful melodies, powerful lyrics, and innovative
+        compositions that paint a vibrant picture of artistic expression.
+        Explore the melodies that define moments and narratives that resonate,
+        as Kiki invites you on a captivating auditory experience through this
+        portfolio.
+      </span>
+      <div className="worksImgs">
+        <a href="https://music.apple.com/us/album/good-friends-single/1552929337">
+          <img src={goodfriends} alt="" className="worksImg" />
+        </a>
+        <a href="https://music.apple.com/us/album/grave-single/1509413037">
+          <img src={grave} alt="" className="worksImg" />
+        </a>
+        <a href="https://open.spotify.com/album/4x2FrVcLXptxSMb8tEtZMv?si=jamktjeCQP2O56UPgxrQ3A">
+          <img src={aswelay} alt="" className="worksImg" />
+        </a>
+        <a href="https://music.apple.com/us/album/one-heart/1671896308">
+          <img src={oneheart} alt="" className="worksImg" />
+        </a>
+        <a href="https://open.spotify.com/album/1ypCJdz6cOEtXuMnCATIgH?si=kGWec0RNR1us3yxah5Gvpg">
+          <img src={goaldigger} alt="" className="worksImg" />
+        </a>
+        <a href="https://open.spotify.com/album/19UQkovdPfdIpSE5XyAHg0?si=idE-DV8cQrWeREWii1XWBA">
+          <img src={threeshots} alt="" className="worksImg" />
+        </a>
+      </div>
+      <button className="worksBtn">See More</button>
     </section>
-  )
-}
+  );
+};
 
 export default Works;
